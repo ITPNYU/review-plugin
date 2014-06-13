2a0859655b782274f581f98c082ad7c3153ae6c6
Karl Ward <karl@ghostghost.net>
2014-06-13 15:04:17 -0400

    minor changes --KW

--- view/rp-page.php
+++ view/rp-page.php
@@ -1,3 +1,10 @@
 <h2>ITP Review</h2>
 
-<?php echo "test"; ?>
+<?php 
+$rp_gravity = rp_gravity_query('forms/' . get_option('rp_gravity_form') . '/entries'); 
+
+foreach ($rp_gravity['response']['entries'] as $g) {
+  var_dump($g);
+}
+
+?>
