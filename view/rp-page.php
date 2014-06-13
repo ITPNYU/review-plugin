8f6c05530e527662155de5ab9b11dfc8f14d7beb
Karl Ward <karl@ghostghost.net>
2014-06-13 15:07:10 -0400

    testing --KW

--- view/rp-page.php
+++ view/rp-page.php
@@ -1,10 +1,11 @@
 <h2>ITP Review</h2>
 
 <?php 
-$rp_gravity = json_decode(rp_gravity_query('forms/' . get_option('rp_gravity_form') . '/entries'), TRUE); 
+$rp_gravity = json_decode(rp_gravity_query('forms/' . get_option('rp_gravity_form') . '/entries'), TRUE);
+var_dump($rp_gravity);
 
-foreach ($rp_gravity['response']['entries'] as $g) {
-  var_dump($g);
-}
+//foreach ($rp_gravity['response']['entries'] as $g) {
+//  var_dump($g);
+//}
 
 ?>
