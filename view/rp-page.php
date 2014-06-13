ce89aa4030993ade26c9a183f14375b26893908c
Karl Ward <karl@ghostghost.net>
2014-06-13 15:12:53 -0400

    testing --KW

--- view/rp-page.php
+++ view/rp-page.php
@@ -3,8 +3,10 @@
 <?php 
 $rp_gravity_query = rp_gravity_query('forms/' . get_option('rp_gravity_form') . '/entries');
 
+var_dump($rp_gravity_query);
+
 $result = NULL;
-$ret = http_get($rp_gravity_query);
+$ret = http_get($rp_gravity_query, array('Accept' => 'application/json'));
 if ($ret != FALSE) {
   $result = json_decode(http_parse_message($ret)->body, TRUE);
 }
