6604f930d7f01f51fee260270144bd2530540938
Karl Ward <karl@ghostghost.net>
2014-06-17 15:22:41 -0400

    added amount --KW

--- view/rp-page.php
+++ view/rp-page.php
@@ -169,7 +169,8 @@ var createDecision = function(args) {
         'fname': jQuery('div#rp-entry-' + args['entry']).attr('data-rp-entry-fname'),
         'lname': jQuery('div#rp-entry-' + args['entry']).attr('data-rp-entry-lname'),
         'email': jQuery('div#rp-entry-' + args['entry']).attr('data-rp-entry-email'),
-        'account_id': 2
+        'account_id': 2,
+        'amount': 200
       },
       'config': config
     }),
