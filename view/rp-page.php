1cf845885572f69499c879d4e59226fad0869042
Karl Ward <karl@ghostghost.net>
2014-10-10 16:29:16 -0400

    corrected URL in WP API --KW

--- view/rp-page.php
+++ view/rp-page.php
@@ -283,7 +283,7 @@ var rpDecisionButton = function(args) {
 var rpReviewButton = function(args) {
   console.log('review click ' + args['recommendation'] + ' ' + args['entry']);
   jQuery.ajax({
-    url: '<?php echo network_site_url() . '/wp-content/plugins/review-plugin/api/review'; ?>',
+    url: '<?php echo network_site_url() . 'wp-content/plugins/review-plugin/api/review'; ?>',
     data: JSON.stringify({
       'args': {
         'entry_id': args['entry'],
